dbcf00126a10799d1a34dac09897e6fe4cbf2716
Theogoro <theoberta@gmail.com>
2025-11-07 17:12:24 -0300

    Add files via upload

---
+++ src/main/java/com/los_btc_de_la_abuela/dsi/TPI/TpiApplication.java
@@ -0,0 +1,13 @@
+package com.los_btc_de_la_abuela.dsi.TPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/los_btc_de_la_abuela/dsi/TPI/TpiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.los_btc_de_la_abuela.dsi.TPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
